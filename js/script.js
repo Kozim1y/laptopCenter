cb65dc85f0da146427f89644d72e684a6215a6eb
Usta_Kozim <87965501+usta-kozim@users.noreply.github.com>
2022-10-31 10:19:03 +0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let navList = document.querySelector(".nav-list");
+let burgerBtn = document.querySelector(".burger");
+
+burgerBtn.addEventListener("click", ()=> {
+  navList.classList.toggle("nav-list--active");
+})
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  // direction: 'vertical',
+  // loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+  spaceBetween:100,
+
+  // slidesPerView: 1,
+
+
+  loop: true,
+});
