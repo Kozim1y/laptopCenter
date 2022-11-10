12e2dd95efa4a0fd15a596b293be0146e20dcbaf
Usta_Kozim <87965501+usta-kozim@users.noreply.github.com>
2022-11-10 15:44:20 +0500

    Add new js file

---
+++ js/products.js
@@ -0,0 +1,17 @@
+let categoryTitle = document.querySelector(".categorys__title");
+let categorysList = document.querySelector(".categorys__list");
+
+let manufacturersTitle = document.querySelector(".manufacturers__title");
+let manufacturersList = document.querySelector(".manufacturers__list");
+
+let priceTitle = document.querySelector(".price__title");
+let priceList = document.querySelector(".price__list");
+
+let colorsTitle = document.querySelector(".colors__title");
+let colorsList = document.querySelector(".colors__list");
+
+
+toggleClass(categoryTitle, categorysList);
+toggleClass(manufacturersTitle, manufacturersList);
+toggleClass(priceTitle, priceList);
+toggleClass(colorsTitle, colorsList);
